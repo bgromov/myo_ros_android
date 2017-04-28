62481ddd6a2caddafe8beefb354f006b7ed14fcc
Boris Gromov <0xff.root@gmail.com>
2017-04-28 21:43:03 +0200

    WIP

--- myo_node/src/main/java/com/github/bgromov/myo_ros_android/myo_node/SettingsFragment.java
+++ myo_node/src/main/java/com/github/bgromov/myo_ros_android/myo_node/SettingsFragment.java
@@ -1,16 +1,29 @@
 package com.github.bgromov.myo_ros_android.myo_node;
 
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.thalmic.myo.Hub;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -20,12 +33,27 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_main);
 
+        Preference button = findPreference("attach_myo");
+
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Hub hub = Hub.getInstance();
+                hub.attachToAdjacentMyo();
+                return true;
+            }
+        });
+
+        syncPreferences();
+    }
+
+    public void syncPreferences() {
         PreferenceScreen screen = getPreferenceScreen();
         SharedPreferences sharedPreferences = screen.getSharedPreferences();
 
@@ -76,7 +104,40 @@ public class SettingsFragment extends PreferenceFragment {
                 return true;
             });
 
+            p.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    return true;
+                }
+            });
+
             screen.addPreference(p);
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        Preference button = findPreference("attach_myo");
+
+        if (v != null) {
+            ListView lv = (ListView) v.findViewById(android.R.id.list);
+            lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    if (id != button.getLayoutResource()) {
+                        view.set
+                    }
+                    return false;
+                }
+            });
+        }
+
+        return v;
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        syncPreferences();
     }
 }
